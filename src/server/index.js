bd81f322a631c96bceaf69cf10d9811f5fd98ad2
matthewjmink@gmail.com <mmink@oreillyauto.com>
2020-10-05 16:13:23 -0500

    setup server to use namecheap port

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const routes = require('./routes.js');
 
 const app = express();
 const bodyParser = require('body-parser');
-const { PORT = 3000, NODE_ENV } = process.env;
+const { PORT } = process.env;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +13,5 @@ app.use(bodyParser.json());
 app.use('/api/', routes);
 
 app.listen(PORT, () => {
-    if (NODE_ENV === 'development') {
-        console.log(`server running at http://localhost:${PORT}`)
-    }
+    console.log(`server running at http://localhost:${PORT}`);
 });
