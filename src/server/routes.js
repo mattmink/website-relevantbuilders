918478018cdadda7675a70901f5d25a7d9c02de3
matthewjmink@gmail.com <mmink@oreillyauto.com>
2020-10-05 16:19:01 -0500

    test out namecheap app

--- src/server/routes.js
+++ src/server/routes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 // /api/message/send
 router.post('/message/send', sendMessage);
+router.get('/hello', (req, res) => res.status(200).send('hey'));
 
 module.exports = router;
