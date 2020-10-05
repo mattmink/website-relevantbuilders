f73ddd97d59d80b28a99045364c74c418a31f772
matthewjmink@gmail.com <mmink@oreillyauto.com>
2020-10-05 17:09:42 -0500

    remove temporary code for namecheap testing

--- src/server/routes.js
+++ src/server/routes.js
@@ -5,6 +5,5 @@ const router = express.Router();
 
 // /api/message/send
 router.post('/message/send', sendMessage);
-router.get('/hello', (req, res) => res.status(200).send(process.env.MAILER_HOST));
 
 module.exports = router;
