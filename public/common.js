5f9e64f4a661b220e8d52aab86235174cf5c1158
Matt Mink <matthewjmink@gmail.com>
2022-07-07 21:14:03 +0000

    feat: additional spam check
    * if form is submitted in less than 3 seconds, add anti-spam check field
    * if anti-spam field is invalid, prevent submission

--- public/common.js
+++ public/common.js
@@ -4,6 +4,11 @@ import './assets/scss/style.scss';
 import { error, success } from "./toast";
 import icons from './icons';
 
+const loadedTime = Date.now();
+const footerForm = document.querySelector('#footerForm');
+let antiSpamField;
+let antiSpamAnswer;
+
 const handleLinkClick = function handleLinkClick(e, link) {
     const { pathname } = link;
 
@@ -28,7 +33,7 @@ document.addEventListener('click', (e) => {
 // This prevents falsely triggering the 'input' event in IE11 when the [placeholder] is toggled
 const onInputWrapper = cb => (e) => {
     const { target: input } = e;
-    if(input.prevVal === input.value || !input.prevVal && input.value === '') return;
+    if (input.prevVal === input.value || !input.prevVal && input.value === '') return;
     input.prevVal = input.value;
     cb(e);
 }
@@ -53,9 +58,49 @@ Array.from(document.querySelectorAll('input, select, textarea')).forEach((input)
     }, false);
 });
 
+const randomInt = function randomIntFromInterval(min = 0, max = 10) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const addAntiSpamField = function addAntiSpamField() {
+    const submitButton = footerForm.querySelector('button[type="submit"]');
+    const a = randomInt();
+    const b = randomInt();
+    const container = document.createElement('div');
+
+    antiSpamAnswer = a + b;
+
+    container.classList.add('form-group');
+    container.innerHTML = `
+        <label for="footerFormSpamCheck" class="control-label">Please verify you're human. ${a} + ${b} = [?]</label>
+        <input class="form-control"
+                name="footerFormSpamCheck"
+                id="footerFormSpamCheck"
+                required />
+    `;
+
+    footerForm.insertBefore(container, submitButton);
+    antiSpamField = container.querySelector('#footerFormSpamCheck');
+};
+
 let isSubmitting = false;
 const handleFormSubmit = function handleContactFormSubmitEvent(e) {
+    const submitTime = Date.now();
     e.preventDefault();
+
+    if (submitTime - loadedTime <= 3000 && !antiSpamField) {
+        addAntiSpamField();
+        antiSpamField.scrollIntoView({ behavior: "smooth" });
+        return;
+    }
+
+    if (antiSpamField) {
+        const userAnswer = antiSpamField.value ? Number(antiSpamField.value) : undefined;
+        if (antiSpamAnswer !== userAnswer) {
+            antiSpamField.scrollIntoView({ behavior: "smooth" });
+            return;
+        }
+    }
 
     if (isSubmitting) return;
 
@@ -121,4 +166,4 @@ const handleFormSubmit = function handleContactFormSubmitEvent(e) {
         });
 }
 
-document.querySelector('#footerForm').addEventListener('submit', handleFormSubmit);
+footerForm.addEventListener('submit', handleFormSubmit);
